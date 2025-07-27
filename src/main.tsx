4fe2ad9fe9d56b18b96fe161ead040ceac65dfce
Isaac Isvaldo Bunga <isaacisvaldobunga300@gmail.com>
2025-07-27 01:18:52 +0100

    feat: implement login form component and update login page structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import LoginPage from './pages/LoginPage'
+import LoginPage from './pages/auth/LoginPage'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
